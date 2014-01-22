9779690c8a6591391c058349d7debb56a81b6fae
Hunter Haydel <haydh530@gmail.com>
2014-01-21 21:41:20 -0600

    Index read mark associations.

--- lib/unread_mongoid/read_mark.rb
+++ lib/unread_mongoid/read_mark.rb
@@ -1,8 +1,8 @@
 class ReadMark
   include Mongoid::Document
 
-  belongs_to :readable, :polymorphic => true
-  belongs_to :reader, polymorphic: true
+  belongs_to :readable, polymorphic: true, index: true
+  belongs_to :reader, polymorphic: true, index: true
 
   validates_presence_of :reader_id, :reader_type, :readable_id, :readable_type
   validates :reader_id, uniqueness: { scope: [:reader_type, :readable_id, :readable_type] }
